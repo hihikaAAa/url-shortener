c9164b2457a4ec9706d7ce931f354fcdb5ca8cec
Sergei <heheka800@gmail.com>
2025-08-12 02:00:31 +0300

    Добавил функцию SaveURL

--- internal/storage/sqlite/sqlite.go
+++ internal/storage/sqlite/sqlite.go
@@ -4,8 +4,9 @@ import (
 	"database/sql"
 	"fmt"
 
-	_ "github.com/mattn/go-sqlite3" 
-	)
+	"github.com/hihikaAAa/GoProjects/url-shortener/internal/storage"
+	sqlite3 "github.com/mattn/go-sqlite3"
+)
 
 type Storage struct {
 	db *sql.DB // Коннект до базы
@@ -41,4 +42,25 @@ func New(storagePath string)(*Storage, error){
 		return nil, fmt.Errorf("%s: %w", op, err)
 	}
 	return &Storage{db: db},nil
+}
+
+func (s *Storage) SaveURL(urlToSave string, alias string) (int64, error){
+	const op = "storage.sqlite.SaveURL"
+	stmt,err := s.db.Prepare("INSERT INTO url(url,alias) VALUES(?, ?)")
+	if err != nil{
+		return 0, fmt.Errorf("%s: %w,",op ,err)
+	}
+	res,err := stmt.Exec(urlToSave,alias)
+	if err != nil{
+		if sqliteErr, ok := err.(sqlite3.Error); ok && sqliteErr.ExtendedCode == sqlite3.ErrConstraintUnique{
+			return 0, fmt.Errorf("%s: %w", op, storage.ErrUrlExists)
+		}
+		return 0, fmt.Errorf("%s: %w", op, err)
+	}
+
+	id, err := res.LastInsertId() 
+	if err!=nil{
+		return 0, fmt.Errorf("%s : failed to get last insert id : %w", op, err)
+	}
+	return id, nil
 }
